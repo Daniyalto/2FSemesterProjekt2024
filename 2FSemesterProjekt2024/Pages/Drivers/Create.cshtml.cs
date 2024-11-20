908474f6f84acb5699d01970d80b3c377d26c3fb
Daniyal <Daniyal1604@gmail.com>
2024-11-20 11:06:13 +0100

    Create added

---
+++ 2FSemesterProjekt2024/Pages/Drivers/Create.cshtml.cs
@@ -0,0 +1,28 @@
+using _2FSemesterProjekt2024.Models;
+using _2FSemesterProjekt2024.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _2FSemesterProjekt2024.Pages.Drivers
+{
+    public class CreateModel : PageModel
+    {
+        [BindProperty]
+        public Driver Driver { get; set; }
+        private IDriverService driverService;
+        public CreateModel(IDriverService service)
+        {
+            driverService = service;
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            driverService.AddDriver(Driver);
+            return RedirectToPage("GetDriver");
+        }
+    }
+
+}
