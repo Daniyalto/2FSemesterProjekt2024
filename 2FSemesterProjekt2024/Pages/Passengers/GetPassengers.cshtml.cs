8538778130b4e26ca4c13aeb70474804eb668c9e
Dasekan Allan Karim <dak003@edu.zealand.dk>
2024-11-22 12:40:36 +0100

    update

--- 2FSemesterProjekt2024/Pages/Passengers/GetPassengers.cshtml.cs
+++ 2FSemesterProjekt2024/Pages/Passengers/GetPassengers.cshtml.cs
@@ -7,18 +7,16 @@ namespace _2FSemesterProjekt2024.Pages.Passengers
 {
     public class GetPassengersModel : PageModel
     {
-        public class GetPassengerModel : PageModel
-        {
-            public IEnumerable<Passenger> Passengers { get; set; }
-            private IPassengerService passengerService;
-            public GetPassengerModel(IPassengerService service)
-            {
-                passengerService = service;
-            }
-            public void OnGet()
-            {
-                Passengers = passengerService.GetPassengers();
-            }
+          public IEnumerable<Passenger> Passengers { get; set; }
+          private IPassengerService passengerService;
+          public GetPassengersModel(IPassengerService service)
+          {
+              passengerService = service;
+          }
+          public void OnGet()
+          {
+              Passengers = passengerService.GetPassengers();
+          }
         }
     }
 }
