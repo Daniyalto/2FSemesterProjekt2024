10754de4ecc0b7219d1d1835d350133cfa5528e5
Daniyal <Daniyal1604@gmail.com>
2024-11-22 10:48:14 +0100

    update

--- 2FSemesterProjekt2024/Pages/Passengers/Update.cshtml.cs
+++ 2FSemesterProjekt2024/Pages/Passengers/Update.cshtml.cs
@@ -1,12 +1,42 @@
+using _2FSemesterProjekt2024.Models;
+using _2FSemesterProjekt2024.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace _2FSemesterProjekt2024.Pages.Passengers
+namespace _2FSemesterProjekt2024.Pages.Drivers
 {
     public class UpdateModel : PageModel
     {
-        public void OnGet()
+        private readonly IPassengerService passengerService;
+
+        public UpdateModel(IPassengerService service)
         {
+            passengerService = service;
+        }
+
+        [BindProperty]
+        public Passenger Passenger { get; set; }
+
+        public IEnumerable<Passenger> Passengers { get; set; }
+
+        public IActionResult OnGet(int id)
+        {
+            if (Passengers == null)
+            {
+                return RedirectToPage("./Index");
+            }
+            Passengers = passengerService.GetPassengerById(id);
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            passengerService.UpdatePassenger(Passenger);
+            return RedirectToPage("GetDrivers");
         }
     }
 }
