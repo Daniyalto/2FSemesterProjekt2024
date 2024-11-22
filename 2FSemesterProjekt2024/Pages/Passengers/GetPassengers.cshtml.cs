c7d7a730342edcf9dbff64e026b049e7c3ad0600
Dasekan Allan Karim <dak003@edu.zealand.dk>
2024-11-22 12:23:52 +0100

    go

---
+++ 2FSemesterProjekt2024/Pages/Passengers/GetPassengers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _2FSemesterProjekt2024.Pages.Passengers
+{
+    public class GetPassengersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
