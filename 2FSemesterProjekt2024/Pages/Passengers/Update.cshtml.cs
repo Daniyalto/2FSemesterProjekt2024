d82957226e36fb8041d783e8ade2e97ec532ed47
Daniyal <Daniyal1604@gmail.com>
2024-11-25 10:08:36 +0100

    delete

--- 2FSemesterProjekt2024/Pages/Passengers/Update.cshtml.cs
+++ 2FSemesterProjekt2024/Pages/Passengers/Update.cshtml.cs
@@ -17,19 +17,17 @@ namespace _2FSemesterProjekt2024.Pages.Passengers
         [BindProperty]
         public Passenger Passenger { get; set; }
 
-        //public IEnumerable<Passenger> Passengers { get; set; }
-
-        public IActionResult OnGet(int id)
+        public IActionResult OnGet(int pid)
         {
             if (Passenger != null)
             {
                 return RedirectToPage("/Index");
             }
-            Passenger = passengerService.GetPassengerById(id);
+            Passenger = passengerService.GetPassengerById(pid);
             return Page();
         }
 
-        public IActionResult OnPost()
+        public IActionResult OnPost(int pid)
         {
             if (!ModelState.IsValid)
             {
