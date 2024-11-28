0d1ceec88ca32a1f9754d2b86483c23a36b2ede8
Dasekan Allan Karim <dak003@edu.zealand.dk>
2024-11-28 11:37:44 +0100

    he

--- 2FSemesterProjekt2024/Pages/Bookings/Create.cshtml.cs
+++ 2FSemesterProjekt2024/Pages/Bookings/Create.cshtml.cs
@@ -1,3 +1,5 @@
+using _2FSemesterProjekt2024.Models;
+using _2FSemesterProjekt2024.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,21 @@ namespace _2FSemesterProjekt2024.Pages.Bookings
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public Booking Booking { get; set; }
+        private IBookingService bookingService;
+        public CreateModel(IBookingService service)
         {
+            bookingService = service;
+        }
+        public IActionResult OnPost(Booking booking)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            bookingService.AddBooking(Booking);
+            return RedirectToPage("GetDriver");
         }
     }
 }
