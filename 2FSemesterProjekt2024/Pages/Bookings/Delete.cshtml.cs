3bd9b7bf38574446f801698dd80a4710d48863bb
Dasekan Allan Karim <dak003@edu.zealand.dk>
2024-11-28 11:23:40 +0100

    ye

---
+++ 2FSemesterProjekt2024/Pages/Bookings/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _2FSemesterProjekt2024.Pages.Bookings
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
