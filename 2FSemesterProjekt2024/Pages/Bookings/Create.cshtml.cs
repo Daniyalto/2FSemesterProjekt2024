15b99223911d480f73e2f95445f1382a5ee564ca
Daniyal <Daniyal1604@gmail.com>
2024-11-29 12:14:23 +0100

    passid

--- 2FSemesterProjekt2024/Pages/Bookings/Create.cshtml.cs
+++ 2FSemesterProjekt2024/Pages/Bookings/Create.cshtml.cs
@@ -9,13 +9,33 @@ namespace _2FSemesterProjekt2024.Pages.Bookings
     {
         [BindProperty]
         public Booking Booking { get; set; }
+
+        [BindProperty]
+        public int PassengerId { get; set; }
+
         private IBookingService bookingService;
         public CreateModel(IBookingService service)
         {
             bookingService = service;
         }
+
+        public void OnGet(int pid)
+        {
+            // Store the PassengerId when the page loads
+            PassengerId = pid;
+        }
+
+       
         public IActionResult OnPost(Booking booking)
         {
+            var newBooking = new Booking
+            {
+                PassengerId = this.PassengerId
+            }
+            
+
+
+
             if (!ModelState.IsValid)
             {
                 return Page();
