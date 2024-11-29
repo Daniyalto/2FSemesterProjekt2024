33157dc50d19430d45cffe936a254d51838b1669
Dasekan Allan Karim <dak003@edu.zealand.dk>
2024-11-29 10:41:26 +0100

    g

--- 2FSemesterProjekt2024/Models/Booking.cs
+++ 2FSemesterProjekt2024/Models/Booking.cs
@@ -22,8 +22,6 @@ public partial class Booking
     [StringLength(200)]
     public string DropoffLocation { get; set; } = null!;
 
-    public int Seats { get; set; }
-
     [Column(TypeName = "datetime")]
     public DateTime BookingTime { get; set; }
 
